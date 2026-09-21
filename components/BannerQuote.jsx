fbc41a5218566bcc052bc05facae976493028c3a
unicornfarm5 <linea.moltved@gmail.com>
2026-09-21 11:41:13 +0200

    v smal fix yay

--- components/BannerQuote.jsx
+++ components/BannerQuote.jsx
@@ -3,7 +3,7 @@ export function BannerQuote() {
     <section className="bg-[#b9c5b0] py-20">
       <div className="section-wrap text-center">
         <div className="mx-auto max-w-5xl text-2xl italic leading-tight md:text-2xl md:leading-tight">
-          <p>I&apos;m interested in information architecture, usability, and how to translate user needs into digital solutions.I&apos;m motivated to work with data-driven design and usability in thoughtful, collaborative teams.</p>
+          <p>I&apos;m interested in information architecture, usability, and how to translate user needs into digital solutions. I&apos;m motivated to work with data-driven design and usability in thoughtful, collaborative teams.</p>
         </div>
       </div>
     </section>
